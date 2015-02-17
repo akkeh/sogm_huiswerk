80097f650b2aa2f66e4431ae316d918cfc8d6faf
akkeh <akkeaccu@msn.com>
2015-02-17 16:37:39 +0100

    pthread bijgevoegd

--- blok2/lessen/multithread.cpp
+++ blok2/lessen/multithread.cpp
@@ -2,6 +2,9 @@
 #include <pthread.h>    // threads
 #include <cstdlib>
 #include <unistd.h>     // sleep
+// mutex
+
+pthread_mutex_t testmutex = PTHREAD_MUTEX_INITIALIZER;  // mutex
 
 typedef struct th_arg {
     int id;
@@ -12,7 +15,9 @@ void* first(void* arg) {
     int id = (*(th_arg*)arg).id;
     int sleep_time = (*(th_arg*)arg).sleep_time;
     for(int i=0; i<10; i++) {
-        std::cout << "thread " << id << " time: " << i << std::endl;
+        pthread_mutex_lock(&testmutex);     // testmutex gets locked
+            std::cout << "thread " << id << " time: " << i << std::endl;
+        pthread_mutex_unlock(&testmutex);   // testmutex gets unlocked
         sleep(sleep_time);
     }
     pthread_exit((void*)0);
@@ -22,9 +27,9 @@ void* first(void* arg) {
 int main() {
     pthread_t thr[10];
     th_arg args[10];
-    for(int i=0; i<2; i++) {
+    for(int i=0; i<10; i++) {
         args[i].sleep_time = std::rand() % 10 + 1;
-        args[i].id = i + 1;
+        args[i].id = i;
         if(pthread_create(&thr[i], NULL, first, (void*)&args[i]) != 0) {
             std::cout << "\nerror creating thread!\n";
         }   
